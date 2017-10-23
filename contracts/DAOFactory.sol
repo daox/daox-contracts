619d0a5e512e0a852c3870b3c2a9d797c8456a2d
Kirill <kbulgakov@yahoo.com>
2017-10-23 22:21:38 +0300

    Contracts Refactoring

--- contracts/DAOFactory.sol
+++ contracts/DAOFactory.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.4.11;
 import "./DAO.sol";
 
 contract DAOFactory {
-
+    event DAOCreated(
+        address _adress,
+        string _name,
+        string _description,
+        uint8 _minVote,
+        address[] _participants
+    );
     mapping(address => string) DAOs;
 
     function DAOFactory(){}
 
-    function create(address _address, string _name, string _description, uint8 _minVote, address[] _participants) returns (address) {
+    function create(address _address, string _name, string _description, uint8 _minVote, address[] _participants) {
         address newDAO = new DAO(_address, _name, _description, _minVote, _participants);
         DAOs[newDAO] = _name;
 
-        return newDAO;
+        DAOCreated(_address, _name, _description, _minVote, _participants);
     }
 }
