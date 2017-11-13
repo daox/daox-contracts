3a03444c631b10e5871b96c68c054e445ecbd678
Kirill <kbulgakov@yahoo.com>
2017-11-13 18:48:50 +0300

    Token decimals is now constant

--- contracts/Token/Token.sol
+++ contracts/Token/Token.sol
@@ -6,11 +6,10 @@ import '../../node_modules/zeppelin-solidity/contracts/token/MintableToken.sol';
 contract Token is MintableToken {
     string public name;
     string public symbol;
-    uint public decimals;
+    uint constant public decimals = 18;
 
-    function Token(string _name, string _symbol, uint _decimals) {
+    function Token(string _name, string _symbol) {
         name = _name;
         symbol = _symbol;
-        decimals = _decimals;
     }
 }
