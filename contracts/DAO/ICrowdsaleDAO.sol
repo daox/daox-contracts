6ae33a8250a5452e9179b8fa354dba46943ca700
Kirill <kbulgakov@yahoo.com>
2017-11-29 14:29:09 +0300

    Interface refactoring[2]

---
+++ contracts/DAO/ICrowdsaleDAO.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+
+import "./IDAO.sol";
+
+contract ICrowdsaleDAO is IDAO {
+    function addProposal(string _description, uint _duration, bytes32[] _options) external;
+
+    function addWithdrawal(string _description, uint _duration, uint _sum) external;
+
+    function addRefund(string _description, uint _duration) external;
+
+    function makeRefundable();
+
+    function flushWhiteList() external;
+
+    function changeWhiteList(address _addr, bool res) external;
+
+    function holdTokens(address _address, uint duration) external;
+
+    function withdrawal(address _address, uint withdrawalSum);
+}
