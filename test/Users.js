c940dfdd6aad09d93454f6b4eeca8ac2618b2f32
VityazevAnton <vityazevanton@gmail.com>
2017-12-01 13:37:25 +0300

    fixed test

--- test/Users.js
+++ test/Users.js
@@ -1,27 +1,18 @@
 "use strict";
 const Users = artifacts.require("./Users/Users.sol");
+let users = null;
+
 
 contract("Users", accounts => {
+    beforeEach(() => Users.new().then(_users => users = _users));
     const serviceAccount = accounts[0];
     const unknownUser = accounts[1];
 
     it("Unknown user should not be in Users", () =>
-        Users.deployed()
-            .then(instance => instance.doesExist.call(unknownUser))
-            .then(doesExist => assert.equal(false, doesExist, "Unknown user exists in User contract"))
-    );
+        users.doesExist.call(unknownUser).then(doesExist => assert.equal(false, doesExist, "Unknown user exists in User contract")));
 
-    it("Should register new user", () => {
-        let UserInstance;
-
-        return Users.deployed().then(instance => {
-            UserInstance = instance;
-
-            return UserInstance.registerNewUser("UserName", "UserSecondName", "UserEmail", unknownUser, {from : serviceAccount});
-        })
-            .then(() => UserInstance.doesExist.call(unknownUser))
-            .then(doesExist => assert.equal(true, doesExist, "User should exist after registering"));
-    });
-
-
+    it("Should register new user", () =>
+        users.registerNewUser("UserName", "UserSecondName", "UserEmail", unknownUser, {from: serviceAccount})
+            .then(() => users.doesExist.call(unknownUser))
+            .then(doesExist => assert.equal(true, doesExist, "User should exist after registering")));
 });
