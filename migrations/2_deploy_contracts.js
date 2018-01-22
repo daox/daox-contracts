c427fc2cf22495923bdb4c6129f72fc59c0ace01
Kirill <kbulgakov@yahoo.com>
2018-01-22 20:07:01 +0300

    Fix truffle config (gas limit) && Add latest migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,27 +1,41 @@
 const Common = artifacts.require("./Common.sol");
-const Users = artifacts.require("./Users/Users.sol");
 const Token = artifacts.require("./Token/Token.sol");
 const VotingFactory = artifacts.require("./Votings/VotingFactory.sol");
 const VotingLib = artifacts.require("./Votings/VotingLib.sol");
 const DAOx = artifacts.require("./DAOx.sol");
-const DAO = artifacts.require("./DAO/DAO.sol");
 const DAOLib = artifacts.require("./DAO/DAOLib.sol");
 const CrowdsaleDAOFactory = artifacts.require("./DAO/CrowdsaleDAOFactory.sol");
+const State = artifacts.require("./DAO/Modules/State.sol");
+const Payment = artifacts.require("./DAO/Modules/Payment.sol");
+const VotingDecisions = artifacts.require("./DAO/Modules/VotingDecisions.sol");
+const Crowdsale = artifacts.require("./DAO/Modules/Crowdsale.sol");
+const Voting = artifacts.require("./DAO/Votings/Voting.sol");
+const DAODeployer = artifacts.require("./DAO/DAODeployer.sol");
+const DAOProxy = artifacts.require("./DAO/DAOProxy.sol");
 
-module.exports = function (deployer) {
-    Promise.all([
-        deployer.deploy(Common),
-        deployer.deploy(VotingLib),
+module.exports = (deployer) => {
+    const deployVotingFactory = () =>
+        deployer.deploy(Common)
+            .then(() => deployer.link(Common, Voting) && deployer.deploy(Voting) && deployer.deploy(VotingLib))
+            .then(() => deployer.link(VotingLib, VotingFactory) && deployer.deploy(VotingFactory, Voting.address));
+
+    const deployDAOx = () =>
+        deployer.deploy(DAOx);
+
+    const deployModules = () =>
         deployer.deploy(DAOLib)
-    ]).then(() => Promise.all([
-        deployer.link(Common, [VotingFactory, Users, CrowdsaleDAOFactory]),
-        deployer.link(VotingLib, VotingFactory),
-        deployer.link(DAOLib, CrowdsaleDAOFactory),
-    ])).then(() => Promise.all([
-        deployer.deploy(Users),
-        deployer.deploy(Token),
-        deployer.deploy(VotingFactory),
-        deployer.deploy(DAOx),
-        deployer.deploy(DAO)
-    ])).then(() => deployer.deploy(CrowdsaleDAOFactory, Users.address, DAOx.address, VotingFactory.address, DAO.address));
+            .then(() => deployer.deploy(State))
+            .then(() => deployer.link(DAOLib, [Payment, Crowdsale]) && deployer.deploy(Payment))
+            .then(() => deployer.deploy(VotingDecisions))
+            .then(() => deployer.deploy(Crowdsale));
+
+    const deployCrowdsaleDAOFactory = () =>
+        deployer.link(Common, [CrowdsaleDAOFactory, DAODeployer]) && deployer.deploy(DAOProxy)
+            .then(() => deployer.link(DAOLib, DAODeployer) && deployer.link(DAOProxy, DAODeployer) && deployer.deploy(DAODeployer))
+            .then(() => deployer.link(DAODeployer, CrowdsaleDAOFactory))
+            .then(() => deployer.deploy(CrowdsaleDAOFactory, DAOx.address, VotingFactory.address, [State.address, Payment.address, VotingDecisions.address, Crowdsale.address]));
+
+    return Promise.all([deployVotingFactory(), deployDAOx(), deployModules()])
+        .then(() => deployCrowdsaleDAOFactory())
 };
+
