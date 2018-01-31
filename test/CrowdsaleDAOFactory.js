e04f9cc08dffb98e5d3f350100e5f2b7b56cfbeb
Kirill <kbulgakov@yahoo.com>
2018-01-31 17:50:49 +0300

    [Test] Add CrowdsaleDAOFactory tests

--- test/CrowdsaleDAOFactory.js
+++ test/CrowdsaleDAOFactory.js
@@ -6,14 +6,36 @@ contract("CrowdsaleDAOFactory", accounts => {
     const serviceAccount = accounts[0];
 
     let cdf;
-    beforeEach(() => helper.createCrowdsaleDAOFactory(accounts).then(_cdf => cdf = _cdf));
+    beforeEach(async () => cdf = await helper.createCrowdsaleDAOFactory(accounts));
 
-    it("Unknown dao should not be in Factory", () =>
-        cdf.exists.call(accounts[0])
-            .then(doesExist => assert.equal(false, doesExist, "Unknown DAO exists in User contract")));
+    it("Unknown dao should not be in Factory", async () => {
+        const result = await cdf.exists.call(accounts[0]);
 
-    it("Should create DAO", () =>
-        helper.createCrowdsaleDAO(cdf, accounts).then(cdf => cdf.exists.call(cdf.dao._address))
-            .then(doesExist => assert.equal(true, doesExist, "Created crowdsale DAO should exist")));
+        assert.equal(false, result, "Unknown DAO exists in User contract");
+    });
+
+    it("Should create DAO with correct fields", async () => {
+        const [daoName, daoDescription] = ["DAO NAME", "THIS IS A DESCRIPTION"];
+        const dao = await helper.createCrowdsaleDAO(cdf, accounts, [daoName, daoDescription]);
+        const result = await cdf.exists.call(dao.address);
+
+        assert.equal(true, result, "Created crowdsale DAO should exist");
+        assert.equal(daoName, await dao.name.call(), "DAO's name doesn't correspond to the expected");
+        assert.equal(helper.fillZeros(web3.toHex(daoDescription)), await dao.description.call(), "DAO's description doesn't correspond to the expected");
+    });
+
+    it("Should set correct parameters in initState", async () => {
+        const [daoName, daoDescription] = ["DAO NAME", "THIS IS A DESCRIPTION"];
+        const dao = await helper.createCrowdsaleDAO(cdf, accounts, [daoName, daoDescription]);
+        const [daoxAddress, votingFactoryAddress, token] = await Promise.all([cdf.serviceContractAddress.call(), cdf.votingFactoryContractAddress.call(), helper.createToken("ANTOKEN", "ANT")]);
+
+        const tx = await dao.initState.sendTransaction(token.address, votingFactoryAddress, daoxAddress, {
+            from : serviceAccount
+        });
+
+        assert.equal(daoxAddress, await dao.serviceContract.call());
+        assert.equal(votingFactoryAddress, await dao.votingFactory.call());
+        assert.equal(token.address, await dao.token.call());
+    })
 
 });
