eed0a61214a3fd8ede34463ce2bae532d3d44edd
VityazevAnton <vityazevanton@gmail.com>
2018-02-27 17:16:38 +0300

    Fix voting cards and dao creation logic

--- contracts/Votings/VotingInterface.sol
+++ contracts/Votings/VotingInterface.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.0;
 
 interface VotingInterface {
     function finished() constant returns(bool);
+
     function voted(address _address) constant returns (uint);
 
     function getOptions() external constant returns(uint[2] result);
