e6d4ab4091089683df5b4c06a1957f59b297cfa0
kirill <kbulgakov@yahoo.com>
2018-05-04 10:17:19 +0300

    [Contract] Add doc for `contributeTo` function

--- contracts/Token/DXC.sol
+++ contracts/Token/DXC.sol
@@ -15,7 +15,12 @@ contract DXC is MintableToken {
     string public constant symbol = "DXC";
     uint public constant decimals = 18;
 
-
+    /**
+     * @dev Transfer specified amount of tokens to the specified address and call
+     * standard `handleDXCPayment` method of Crowdsale DAO
+     * @param _to The address of Crowdsale DAO
+     * @param _amount The amount of tokens to send
+    */
     function contributeTo(address _to, uint256 _amount) public {
         super.transfer(_to, _amount);
         require(_to.call(bytes4(keccak256("handleDXCPayment(address,uint256)")), msg.sender, _amount));
